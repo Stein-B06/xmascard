3d77832625efea920fd62a104309b44548ce0a98
Stein-B06 <Steinb06.kodehode@gmail.com>
2024-12-17 13:37:41 +0100

    title, sm. change comp.

--- src/components/Snow.tsx
+++ src/components/Snow.tsx
@@ -5,13 +5,12 @@ const Snow: React.FC = () => {
     return Array.from({ length: 80 }, (_, i) => (
       <div
         key={i} // Unik nøkkel for hvert snøflak basert på indeks
-        className="snowflake"
+        className="snowflake text-xs md:text-base"
         style={{
           left: `${Math.random() * 120}vw`, // Tilfeldig horisontal plassering over skjermen
           animationDelay: `${Math.random() * 10}s`, // Tilfeldig forsinkelse for å spre snøflakene over tid
           animationDuration: `${5 + Math.random() * 20}s`, // Varierende fallhastighet
           color: "white",
-          fontSize: "12px",
         }}
       >
         ❄
